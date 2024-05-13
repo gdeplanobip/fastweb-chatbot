659dd7d19f8ac4eb143f09b1df888a464cbb6ec6
gdeplanobip <giampaolo.deplano@bip-group.com>
2024-05-13 18:17:20 +0200

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,154 @@
+""" 
+TO RUN YOUR APP USE THIS COMMAND ON TERMINAL
+-> streamlit run chatbot_duble.py
+"""
+
+import streamlit as st
+import boto3
+import json
+import json
+import logging
+import time
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.INFO)
+
+TOKEN_BLACKLIST = { # id: text
+    13: '\n',
+    733: ' [', 
+    28766: '|',
+    11741: 'AI',
+    28766: '|',
+    28793: ']',
+    28792: '[',
+    28779: 'U',
+    1294: 'man',
+    28709: 'o'
+}
+
+STOPWORD_SEQ_1 = ['\n', '[', '|', 'U', 'man','o']
+STOPWORD_SEQ_2 = ['\n', ' [', '|', 'U', 'man', 'o']
+
+SKIPWORD_SEQ_1 = ['\n', '[', '|', 'AI', '|', ']']
+SKIPWORD_SEQ_2 = ['\n', ' [', '|', 'AI', '|', ']']
+
+
+class History:
+    def __init__(self):
+        self.history = []
+    
+    def add(self, subject, message):
+        if len(self.history) == 0:
+            self.history.append(f"Conversazione tra umano ed AI, [|{subject}|] {message}")
+        else:
+            self.history.append(f"[|{subject}|] {message}")
+    
+    def format(self):
+        return "\n ".join(self.history)
+
+
+def response_generator(a):
+    stop_sequence = []
+    tokens = []
+    for p in a["Body"]:
+        try:
+            token = json.loads(p["PayloadPart"]['Bytes'].decode('utf-8').split('data:')[1])['token']
+            tokens.append(token['text'])
+            if any([token['id'] == x for x in TOKEN_BLACKLIST.keys()]):
+                stop_sequence.append(token['text'])
+                if len(stop_sequence) >= 6 and (stop_sequence[-6:] == STOPWORD_SEQ_1 or stop_sequence[-6:] == STOPWORD_SEQ_2):
+                    logging.info('Risposta bloccata per rilevazione stopword')
+                    return()
+                if len(stop_sequence) >= 6 and (stop_sequence[-6:] == SKIPWORD_SEQ_1 or stop_sequence[-6:] == SKIPWORD_SEQ_2):
+                    logging.info('Risposta accorciata per rilevazione skipword')
+                    tokens = []
+            if len(tokens) == 6:
+                token_return = tokens[0]
+                tokens = tokens[1:]
+                yield token_return
+        except:
+            pass
+
+def dumb_response_generator(sentence):
+    for word in sentence.split():
+        yield word + " "
+        time.sleep(0.05)
+
+
+client = boto3.client("runtime.sagemaker", 
+                        aws_access_key_id=st.secrets["aws_access_key_id"] 
+                        aws_secret_access_key=st.secrets["aws_secret_access_key"] 
+                        aws_session_token=st.secrets["aws_session_token"]
+                        
+with st.sidebar:
+
+    if st.button("Nuova conversazione", type="primary"):
+        for key in st.session_state.keys():
+            del st.session_state[key]
+        logging.info("Reset storico conversazione per scelta dell'utente")
+
+    st.divider()
+
+    model = st.selectbox(
+        "Selezione il modello con cui interagire",
+        ("Mistral", "Gemma"))
+
+
+# if model == 'Mistral':
+logging.info(f'Modello scelto: {model}')
+st.title("LLM NAZIONALE")
+st.header(model)   
+
+# if model == 'Gemma': 
+#     logging.info(f'Modello scelto: {model}')
+#     st.title("LLM NAZIONALE")
+
+
+if "history" not in st.session_state:
+    st.session_state["history"] = History()
+    logging.info('Inizializzazione storico conversazione')
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("Scrivi.."):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+        st.session_state["history"].add(subject="Umano", message=prompt)
+        message = st.session_state["history"].format()
+
+    payload = {
+        "inputs": message,
+        "parameters": {"max_new_tokens": 256, "stop":["[|Umano|]"]},
+        "stream": True
+        }
+
+    if model == 'Mistral':
+        with st.chat_message("assistant"):
+            stream = client.invoke_endpoint_with_response_stream(
+                EndpointName="llm-nazionale-mistral-demo13052024",
+                Body=json.dumps(payload),
+                ContentType="application/json")
+        
+            response = st.write_stream(response_generator(stream))
+    
+    elif model == 'Gemma':
+        with st.chat_message("assistant"):
+            stream = client.invoke_endpoint_with_response_stream(
+                EndpointName="llm-nazionale-mistralMount-demo13052024",
+                Body=json.dumps(payload),
+                ContentType="application/json")
+        
+            response = st.write_stream(response_generator(stream))
+
+    st.session_state.messages.append({"role": "assistant", "content": response})
+    st.session_state["history"].add(subject="AI", message=response)
+    print("------ OUTPUT --------")
+    print(st.session_state["history"].format())
+    print()
+
