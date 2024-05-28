eb02906d4842b1e1543c09ba0be33663c36853fd
gdeplanobip <giampaolo.deplano@bip-group.com>
2024-05-28 12:49:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -117,8 +117,12 @@ if "messages" not in st.session_state:
     st.session_state.messages = []
 
 for message in st.session_state.messages:
-    with st.chat_message(message["role"], avatar=IMAGE_URL):
-        st.markdown(message["content"])
+    if message["role"] != "user":
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+    else:
+        with st.chat_message(message["role"], avatar=IMAGE_URL):
+            st.markdown(message["content"])
 
 if prompt := st.chat_input("Scrivi.."):
     st.session_state.messages.append({"role": "user", "content": prompt})
