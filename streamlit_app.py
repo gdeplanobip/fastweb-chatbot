af02de5cb5ffaef80e65704eaa64d4a2e5999758
gdeplanobip <giampaolo.deplano@bip-group.com>
2024-06-05 11:59:23 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,11 +60,13 @@ client = OpenAI(
     base_url=OPENAI_API_BASE,
 )
 
-with st.chat_message("assistant", avatar=BOT_LOGO_URL):
-    benv = "Ciao sono Enea, il nuovo assistente generativo di Fastweb, cosa posso fare per te?"
-    response = st.write_stream(benvenuto(benv))
-    st.session_state.messages.append({"role": "assistant", "content": benv})
-    # st.session_state["history"].add(subject="assistant", message=benv)
+# with st.chat_message("assistant", avatar=BOT_LOGO_URL):
+#     benv = "Ciao sono Enea, il nuovo assistente generativo di Fastweb, cosa posso fare per te?"
+#     response = st.write_stream(benvenuto(benv))
+#     st.session_state.messages.append({"role": "assistant", "content": benv})
+#     # st.session_state["history"].add(subject="assistant", message=benv)
+
+st.text("Ciao sono Enea, il nuovo assistente generativo di Fastweb, cosa posso fare per te?")
 
 with st.sidebar:
     st.image(LOGO_URL)
