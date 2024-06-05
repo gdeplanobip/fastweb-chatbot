f3932715aff5b46f527bd686346e17ac111195bc
gdeplanobip <giampaolo.deplano@bip-group.com>
2024-06-05 11:54:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -130,11 +130,13 @@ with response_container:
 
 
     else:
+        st.session_state["input_disabled"] = True
         with st.chat_message("assistant", avatar=BOT_LOGO_URL):
             benv = "Ciao sono Enea, il nuovo assistente generativo di Fastweb, cosa posso fare per te?"
             response = st.write_stream(benvenuto(benv))
             st.session_state.messages.append({"role": "assistant", "content": benv})
-            st.session_state["history"].add(subject="assistant", message=benv)
+            # st.session_state["history"].add(subject="assistant", message=benv)
+        st.session_state["input_disabled"] = False
 
     logging.info(f'{st.session_state["history"].history}')
 
