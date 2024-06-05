15415d30fec20d68f34b5a8a4e2845b9775d01e2
gdeplanobip <giampaolo.deplano@bip-group.com>
2024-06-05 14:01:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,8 @@ logging.getLogger().setLevel(logging.INFO)
 LOGO_URL = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Fastweb_logo.svg/2560px-Fastweb_logo.svg.png"
 BOT_LOGO_URL = "https://www.fastweb.it/myfastweb/gfx/common/app-icon-2023@2x.png"
 
-MODEL_URL = "https://eadaee78c5feaaae48433fcde17458cb.serveo.net"
+MODEL_URL = "https://db94836ad2fc8dfd3572263138ba5a94.serveo.net"
+MODEL_NAME = "Fastweb/Enea-v0.5-llama3-8b"
 OPENAI_API_KEY = "EMPTY"
 OPENAI_API_BASE = f"{MODEL_URL}/v1"
 ARGS = {
@@ -58,7 +59,7 @@ client = OpenAI(
 
 with st.sidebar:
     st.image(LOGO_URL)
-    st.markdown("<h1 style='text-align: right; color: #fdc500;'>Enea v0.4</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: right; color: #fdc500;'>Enea v0.5</h1>", unsafe_allow_html=True)
 
     add_vertical_space(35)
     
@@ -117,7 +118,7 @@ with response_container:
 
         with st.chat_message("assistant", avatar=BOT_LOGO_URL).empty():
             stream = client.chat.completions.create(
-                model="Fastweb/Enea-v0.4-llama3-8b",
+                model=MODEL_NAME,
                 messages=st.session_state["history"].history,
                 stream=True,
                 **ARGS)     
